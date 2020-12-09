04a064fc42e386c80f5d16bbe52c93c08a4716cc
Bernardo Aguayo Ortega <bernardoaguayoortega@pop-os.localdomain>
2020-12-09 17:17:25 -0600

    Create message component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import Message from "./Message";
 
 function App() {
   const tuple: [string, number] = ["Bernie", 12];
 
-  return <div className="App">
-    {tuple[1]}
-  </div>;
+  return (
+    <div className="App">
+      {tuple[1]}
+      <Message />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Message = () => {
+  return <p>Htis is a quick message</p>;
+};
+
+export default Message;
